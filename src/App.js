166b8a76765e602364c3292c1a72b2deea2521f9
LEEJONGHWA <unknown9732@naver.com>
2020-05-14 20:59:30 +0900

    O,X 응답 로직 개선

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from 'react';
 import Container from "./Container";
 import Blocks from "./Blocks";
+import Button from "@material-ui/core/Button";
 
 function makeRandom() {
     const rand = [];
@@ -14,29 +15,29 @@ function makeRandom() {
 const App = () => {
 
     const [start, setStart] = useState(true); // 입력 받기 전
-    const [num, setNum] = useState(12); // Container에 전달할 props
-    const [flag, setFlag] = useState(false);
-    const [result, setResult] = useState([]);
-    const rand = useRef(makeRandom());
+    const [num, setNum] = useState(12); // Container에 전달할 props. 12는 위치 표시 OFF 상태 의미
+    const [flag, setFlag] = useState(false); // 응답 여부 플래그
+    const [result, setResult] = useState([]); // O,X 응답 배열
+    const rand = useRef(makeRandom()); // 난수 배열
+    const idx = useRef(0); // 난수 배열 인덱스
 
     const handleOnClick = () => {
         setStart(false);
-        let intervalCnt = 0;
 
         const interval = setInterval(() => {
-            setNum(rand.current[intervalCnt]);
-            console.log(rand.current[intervalCnt] + " 켜기");
+            setNum(rand.current[idx.current]);
+            console.log(rand.current[idx.current] + " 켜기");
             setFlag(true);
 
 
             setTimeout(() => {
                 setNum(12);
-                intervalCnt++;
                 console.log("위치 표시 끄기");
                 setFlag(false);
+                idx.current++;
             }, 1000)
 
-            if (intervalCnt === 5) {
+            if (idx.current === 5) {
                 clearInterval(interval);
                 console.log(" 인터벌 종료")
             }
@@ -46,7 +47,7 @@ const App = () => {
 
     const handleAnswerOnClick = (flag, type) => {
         if (flag) {
-            setResult(result.concat(type));
+            setResult(result.concat({[idx.current]: type}));
             console.log(type + ' 푸시')
             setFlag(false);
         }
@@ -68,9 +69,9 @@ const App = () => {
                     <div>
                         <Container num={num}>
                             <Blocks/>
-                            <button style={{gridColumn: '1 / 3', marginRight: "3rem"}} onClick={() => handleAnswerOnClick(flag, 'O')}>O</button>
-                            <button style={{gridColumn: '3 / 5', marginLeft: "3rem"}} onClick={() => handleAnswerOnClick(flag, 'X')}>X</button>
-                            <button style={{gridRow: '5', gridColumn:'2 / 4'}} onClick={() => console.log(result)}>결과보기</button>
+                            <Button style={{gridColumn: '1 / 3', marginRight: "3rem"}} onClick={() => handleAnswerOnClick(flag, 'O')}>O</Button>
+                            <Button style={{gridColumn: '3 / 5', marginLeft: "3rem"}} onClick={() => handleAnswerOnClick(flag, 'X')}>X</Button>
+                            <Button style={{gridRow: '5', gridColumn:'2 / 4'}} onClick={() => console.log(result)}>결과보기</Button>
                         </Container>
                     </div>
             }
---
+++ src/ShowResult.js
@@ -0,0 +1,5 @@
+export default (N, rand, result) => {
+    //TODO
+    // N, rand, result를 받아서 결과를 출력
+
+}
